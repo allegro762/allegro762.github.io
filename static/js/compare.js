dc9d4f0aab2f7e416ee3b62d219ea6f973bf5884
isin <126842518+allegro762@users.noreply.github.com>
2026-04-08 15:30:47 +0900

    Update compare.js

--- static/js/compare.js
+++ static/js/compare.js
@@ -1,8 +1,12 @@
 console.log("JS loaded");
 
 const categories = {
-  "신체부위": ["eye", "hair", "face", "arm", "leg"],
-  "의류": ["shirt", "skirt", "pants", "jacket"],
+  "신체부위": ["eyes", "eye", "hair", "face", "arm", "leg", "breast", "breasts", "legs"],
+  "헤어": ["hair"],
+  "의류": ["shirt", "skirt", "pants", "jacket", "thighhighs", "sleeves", "sweater", ],
+  "이미지 후처리": ["lighting", "anime style", "detailed", "quality", "score"],
+  "디테일": ["striped", "leather", 
+  "악세사리": ["bow", ],
   "기타": []
 };
 
